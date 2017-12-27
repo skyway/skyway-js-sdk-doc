3d88949e81d000529a5604f40f8cdbdf0d4fdf39
Ryosuke Otsuya <r.otsuya@gmail.com>
2017-12-27 12:34:27 +0900

    add backlinks in left navigation

--- docs/javascript/custom.js
+++ docs/javascript/custom.js
@@ -19,8 +19,12 @@ document.addEventListener('DOMContentLoaded', function(){
   baseHtml = document.querySelector('.md-nav__list');
   customHtml
     = '<hr>'
-    + '<li class="md-nav__item ja-item"><a href="https://webrtc.ecl.ntt.com/" class="md-nav__link">SkyWayに戻る</li>'
-    + '<li class="md-nav__item en-item"><a href="https://webrtc.ecl.ntt.com/en/" class="md-nav__link">back to ECLWebRTC</li>';
+    + '<li class="md-nav__item ja-item hidden-root-item"><a href="https://webrtc.ecl.ntt.com/js-sdk.html" class="md-nav__link backlink"><span class="fa fa-arrow-left"></span> JavaScript SDKに戻る</li>'
+    + '<li class="md-nav__item ja-item hidden-root-item"><a href="https://webrtc.ecl.ntt.com/developer.html" class="md-nav__link backlink"><span class="fa fa-arrow-left"></span> 開発者に戻る</li>'
+    + '<li class="md-nav__item ja-item"><a href="https://webrtc.ecl.ntt.com/" class="md-nav__link backlink"><span class="fa fa-home"></span> SkyWayに戻る</li>'
+    + '<li class="md-nav__item en-item hidden-root-item"><a href="https://webrtc.ecl.ntt.com/en/js-sdk.html" class="md-nav__link backlink"><span class="fa fa-arrow-left"></span> back to JavaScript SDK</li>'
+    + '<li class="md-nav__item en-item hidden-root-item"><a href="https://webrtc.ecl.ntt.com/en/developer.html" class="md-nav__link backlink"><span class="fa fa-arrow-left"></span> back to Developer</li>'
+    + '<li class="md-nav__item en-item"><a href="https://webrtc.ecl.ntt.com/en/" class="md-nav__link backlink"><span class="fa fa-home"></span> back to ECLWebRTC</li>';
   baseHtml.innerHTML += customHtml;
 
   // Switch the social link on footer.
